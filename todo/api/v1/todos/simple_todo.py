64ee7ffd143d933ed8f173dade5e1a950516d5d9
MichaelBidencopeCP <mbidencope3@gmail.com>
2025-08-29 10:03:36 -0400

    tested github actions and fixed test

--- todo/api/v1/todos/simple_todo.py
+++ todo/api/v1/todos/simple_todo.py
@@ -6,7 +6,7 @@ router = APIRouter()
 
 @router.get("/health")
 async def health_check():
-    return {"status": "not ok"}
+    return {"status": "ok"}
 
 @router.get("/todos")
 async def get_todos() -> list[TodoItem]:
